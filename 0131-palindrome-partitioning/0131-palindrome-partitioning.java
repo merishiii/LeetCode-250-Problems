3180385a7c6a3cd2a2260f6061b4cd5899efabc8
Rishabh Pathak <99328708+merishiii@users.noreply.github.com>
2026-09-07 23:52:41 +0530

    Time: 6 ms (98.63%), Space: 64.9 MB (95.04%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,35 +1,32 @@
 class Solution {
     public List<List<String>> partition(String s) {
+        int n = s.length();
+        boolean[][] palindrome = new boolean[n][n];
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i; j < n; j++) {
+                palindrome[i][j] = s.charAt(i) == s.charAt(j)
+                        && (j - i < 2 || palindrome[i + 1][j - 1]);
+            }
+        }
+
         List<List<String>> result = new ArrayList<>();
-        backtrack(s, 0, new ArrayList<>(), result);
+        backtrack(s, 0, palindrome, new ArrayList<>(), result);
         return result;
     }
 
-    private void backtrack(String s, int start, List<String> path, List<List<String>> result) {
+    private void backtrack(String s, int start, boolean[][] palindrome, List<String> path, List<List<String>> result) {
         if (start == s.length()) {
             result.add(new ArrayList<>(path));
             return;
         }
 
         for (int end = start; end < s.length(); end++) {
-            if (isPalindrome(s, start, end)) {
+            if (palindrome[start][end]) {
                 path.add(s.substring(start, end + 1));
-                backtrack(s, end + 1, path, result);
+                backtrack(s, end + 1, palindrome, path, result);
                 path.remove(path.size() - 1);
             }
         }
-    }
-
-    private boolean isPalindrome(String s, int left, int right) {
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right)) {
-                return false;
-            }
-
-            left++;
-            right--;
-        }
-
-        return true;
     }
 }
